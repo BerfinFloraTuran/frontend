9a06e9b635aa58ad68cd6d6433bf1c04e19e38ba
Flora <s215709@student.dtu.dk>
2023-02-24 11:50:55 +0100

    css grid sat op

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,24 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={"grid-container1"}>
+          <header className={'grid-header'}>
+              <h2>Header</h2>
+          </header>
+
+          <nav className={'grid-nav'}>
+              <h2>Nav</h2>
+          </nav>
+
+          <section className={'grid-basket'}>
+
+          </section>
+
+          <section className={'grid-total'}>
+
+          </section>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
