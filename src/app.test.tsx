b064832763c5183cab136ea944e5a3f0076723e0
s172488 <55982247+JacobSylvest@users.noreply.github.com>
2023-03-11 14:12:36 +0100

    inital test created.

---
+++ src/app.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import App from "../App";
+
+describe (App.name, ()=> {
+    it("should render", () =>{
+        render (<App/>);
+        expect (screen.getByText ("Subtotal")).toBeInTheDocument();
+    
+    })
+
+
+})
