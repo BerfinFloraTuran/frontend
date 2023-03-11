dd545c5f5adb007678e0be5b4abeee6e103de671
Flora <s215709@student.dtu.dk>
2023-03-11 14:27:26 +0100

    rework basket

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,124 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React, {useEffect} from 'react'
+import {useState} from 'react'
 import './App.css'
 import Listpage, {DataItems} from './ListPage'
 import SumofItems from './SumofItems';
 
-
-
+export interface Product {
+    price: number;
+    name: string;
+    quantity: number;
+    wrapped:boolean;
+}
 function App() {
-  const [data,setData] = React.useState<DataItems[]>([]);
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch('https://raw.githubusercontent.com/larsthorup/checkout-data/main/product.json');
-      const data = await response.json();
-      const mappedData = data.map((item: any) => {
-        const mappedItem: DataItems = {
-          id: item.id,
-          name: item.name,
-          quantity: 0,
-          wrapped: false,
-          price: item.price,
-          currency: item.currency,
-        };
-        return mappedItem;
-      });
-      setData(mappedData);
-    }
-    fetchData();
-  }, []);
+    const [productList, setProductList] = useState<Product[]>([]);
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('https://raw.githubusercontent.com/larsthorup/checkout-data/main/product.json');
+            const data = await response.json();
+            const mappedData = data
+                .slice(0, 4)
+                .map((item: any) => {
+                    const mappedItem: Product = {
+                        price: item.price,
+                        name: item.name,
+                        quantity: 1,
+                        wrapped: false
+                    };
+                    return mappedItem;
+                });
+            setProductList(mappedData);
+        }
+
+        fetchData();
+    }, []);
+
+    const removeItem = (index: number) => {
+        const updatedProductList = [...productList];
+        updatedProductList.splice(index, 1);
+        setProductList(updatedProductList);
+    };
+
+    const updateQuantity = (index: number, quantity: number) => {
+        const updatedProductList = [...productList];
+        updatedProductList[index].quantity = quantity;
+        setProductList(updatedProductList);
+    };
 
 
-  return (
-    <div className="App">
-      <div className={"grid-container1"}>
-        
-          <header className={'grid-header'}>
-              <h2>Header</h2>
-          </header>
+    return (
+        <div className="App">
+            <div className={"grid-container1"}>
 
-          <nav className={'grid-nav'}>
-              <h2>Nav</h2>
-              <div className="container">
-  <div className="progress-container">
-    <div className="progress" id="progress"> </div>
-    <div className="circle active">25%</div>
-    <div className="circle">50%</div>
-    <div className="circle">75%</div>
-    <div className="circle">100%</div>
-  </div>
-</div>
-          </nav>
+                <header className={'grid-header'}>
+                    <h2>Header</h2>
+                </header>
 
-          <section className={'grid-basket'}>
-            <h2>Indkøbskurv</h2>
-            <Listpage dataitems={data} onUpdateItems={setData}
-            columNames={['Id','Name','Price','Wrapped','Quantity']}/>
-          </section>
+                <nav className={'grid-nav'}>
+                    <h2>Nav</h2>
+                    <div className="container">
+                        <div className="progress-container">
+                            <div className="progress" id="progress"></div>
+                            <div className="circle active">25%</div>
+                            <div className="circle">50%</div>
+                            <div className="circle">75%</div>
+                            <div className="circle">100%</div>
+                        </div>
+                    </div>
+                </nav>
 
-          <section className={'grid-total'}>
-            <h2>Odreoverblik</h2>
-            <SumofItems dataItems={data}/>
+                <section className={'grid-basket'} id={"basket"}>
+                    <h2>Indkøbskurv</h2>
+                    {productList.map((product, index) => (
+                        <section key={index}>
+                            <div className="product-wrapper">
+                                <img src={"src/assets/noimg.png"} alt={product.name}/>
+                                <div className="product-info">
+                                    <button className="close-button" onClick={() => removeItem(index)}>
+                                        X
+                                    </button>
+                                    <ul>
+                                        <li>{product.price},- pr. stk.</li>
+                                        <li>{product.name}</li>
+                                        <li>
+                                            <label>Antal</label>
+                                            <select
+                                                id={'val'}
+                                                value={product.quantity}
+                                                onChange={(e) =>
+                                                    updateQuantity(index, parseInt(e.target.value))
+                                                }
+                                            >
+                                                {[...Array(10)].map((_, i) => (
+                                                    <option key={i} value={i + 1}>
+                                                        {i + 1}
+                                                    </option>
+                                                ))}
+                                            </select>
+                                        </li>
+                                        <li>
+                                            <label>Gaveindpakning</label>
+                                            <input type={"checkbox"}/>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </section>
+                    ))}
+                </section>
 
-          </section>
+                <section className={'grid-total'}>
+                    <h2>Odreoverblik</h2>
+                    <SumofItems dataItems={productList}/>
 
-          <footer className={"grid-footer"}>
+                </section>
 
-          </footer>
+                <footer className={"grid-footer"}>
 
-      </div>
-    </div>
-  )
+                </footer>
+            </div>
+        </div>
+    )
 }
 
 export default App
--- src/SumofItems.tsx
+++ src/SumofItems.tsx
@@ -1,23 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import React from 'react';
-import { DataItems } from './ListPage';
+import { Product } from './App';
 
-function calculatePrice(products:DataItems[]): { subtotal: number, total: number } {
+function calculatePrice(products:Product[]): { subtotal: number, total: number } {
   const SHIPPING_THRESHOLD = 500;
   const SHIPPING_FEE = 10;
-  let subtotal = 0;
   let total = 0;
-  let countOfItems = 0;
-  products.map((product)=>{
-    subtotal+=product.price * product.quantity;
-    if(product.quantity > 0){
-      countOfItems += product.quantity
-    };
-  } 
-  );
 
-  if(subtotal>500){
+    const subtotal = products.reduce((sum, product) =>
+     sum + product.price * product.quantity, 0);
+
+    if(subtotal>500){
     total = subtotal;
   }else{
     total = subtotal + SHIPPING_FEE;
@@ -29,7 +23,7 @@ function calculatePrice(products:DataItems[]): { subtotal: number, total: number
 }
 
 interface Props{
-  dataItems:DataItems[]
+  dataItems:Product[]
 }
 
 
