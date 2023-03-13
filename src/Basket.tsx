d89c76428a192759010c09e281d9c18aaf7cc1f4
Flora <s215709@student.dtu.dk>
2023-03-13 21:09:37 +0100

    upsell product replaced

--- src/Basket.tsx
+++ src/Basket.tsx
@@ -1,10 +1,11 @@
-import React, {useEffect} from 'react'
+import React, {ChangeEvent, ChangeEventHandler, useEffect} from 'react'
 import {useState} from 'react'
 import './Basket.css'
 import SumofItems from './SumofItems';
 import {PostalCodeinput} from './InputAssets/PostalCode';
 
 export interface Product {
+    id: string;
     price: number;
     name: string;
     quantity: number;
@@ -19,21 +20,56 @@ interface Props {
 
 function Basket(props: Props) {
     const [productList, setProductList] = useState<Product[]>(props.productList);
-
-    function handleCheckboxChange() {
-        setIsChecked(!isChecked);
-    }
-
     const [isChecked, setIsChecked] = useState(false);
+    const [isPopupVisible, setIsPopupVisible] = useState(false);
+    const [selectedProductIndex, setSelectedProductIndex] = useState(0);
+    const [selectedProductId, setSelectedProductId] = useState<string | undefined>(undefined);
+
+    function getUpsellProduct(productId : string) {
+        const product = productList.find(p => p.id === productId);
+        if (product && product.upsellProductId) {
+            return product.upsellProductId;
+        }
+        return null;
+    }
+    const handleCheckboxChange = (index: number, upsellId: string): ChangeEventHandler<HTMLInputElement> => {
+        return (event: React.ChangeEvent<HTMLInputElement>) => {
+            setIsChecked(currentValue => !currentValue);
+            setSelectedProductIndex(index);
+            setIsPopupVisible(!isPopupVisible);
+            setSelectedProductId(upsellId)
+        };
+    };
+
+    const handlePopupCloseClick = () => {
+        setIsPopupVisible(false);
+        setIsChecked(false);
+        setSelectedProductIndex(0);
+    };
+
+   async function findProductById(id: string | undefined): Promise<Product> {
+        const response = await fetch('https://raw.githubusercontent.com/larsthorup/checkout-data/main/product.json');
+        const jsonData = await response.json();
+        const product = jsonData.find((item: any) => item.id === id);
+            return {
+                id: product.id,
+                price: product.price,
+                name: product.name,
+                quantity: product.quantity || 1,
+                wrapped: false,
+                upsellProductId: product.upsellProductId
+            }};
+
     useEffect(() => {
         async function fetchData() {
             const data = props.productList.length === 0
                 ? await fetch('https://raw.githubusercontent.com/larsthorup/checkout-data/main/product.json')
                     .then(response => response.json())
-                    .then(data => data.slice(4, 7))
+                    .then(data => data.slice(17, 20))
                 : props.productList;
 
             const mappedData = data.map((item: any) => ({
+                id: item.id,
                 price: item.price,
                 name: item.name,
                 quantity: item.quantity || 1,
@@ -54,8 +90,16 @@ function Basket(props: Props) {
 
     const updateQuantity = (index: number, quantity: number) => {
         const updatedProductList = [...productList];
-        updatedProductList[index].quantity = quantity;
+        updatedProductList[selectedProductIndex].quantity = quantity;
         setProductList(updatedProductList);
+    };
+
+    const updateItem = async (index: number, productId: string | undefined) => {
+        const updatedProductList = [...productList];
+        const product = await findProductById(selectedProductId);
+        updatedProductList[selectedProductIndex] = product
+        setProductList(updatedProductList);
+        handlePopupCloseClick();
     };
 
     return (
@@ -65,7 +109,6 @@ function Basket(props: Props) {
                 <header className={'grid-header'}>
                     <h2>Header</h2>
                 </header>
-
                 <div className="grid-nav">
                     <div className="progress" id="progress"></div>
                     <div className="circle active">1</div>
@@ -85,7 +128,6 @@ function Basket(props: Props) {
                                     X
                                 </button>
                                 <div className="product-info">
-
                                     <ul id={"itemslist"}>
                                         <br/>
                                         <li>{product.price},- pr. stk.</li>
@@ -108,7 +150,7 @@ function Basket(props: Props) {
                                         </li>
                                         <li>
                                             <label>Gaveindpakning</label>
-                                            <input id={"check"} type={"checkbox"} data-testid="checkboxTest"/>
+                                            <input id={`giftwrapcheck-${index}`} type={"checkbox"} data-testid="checkboxTest"/>
                                         </li>
                                         {product.upsellProductId && (
                                             <li>
@@ -119,12 +161,29 @@ function Basket(props: Props) {
                                                             .join(' ')}
                                                     ?
                                                 </label>
-                                                <input id={"check"} type={"checkbox"}/>
-
+                                                <input
+                                                    id={`check-${index}`}
+                                                    type={"checkbox"}
+                                                    onChange={handleCheckboxChange(index, product.upsellProductId)}
+                                                />
                                             </li>
                                         )}
                                         <br/>
                                     </ul>
+                                    {isPopupVisible && (
+                                        <div className="overlay" style={{ display: isPopupVisible ? 'flex' : 'none' }}>
+                                            <div className="modal">
+                                                <button className="close-button" onClick={handlePopupCloseClick}>
+                                                    X
+                                                </button>
+                                                <h3>Tak for din bestilling!</h3>
+                                                <p>Din ordre er blevet modtaget.</p>
+                                                <button onClick={(e) =>
+                                                    updateItem(index, product.upsellProductId)
+                                                }>OK</button>
+                                            </div>
+                                        </div>
+                                    )}
                                 </div>
                             </div>
                         </section>
